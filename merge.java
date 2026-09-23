9057a15d527d530cbf200af82cbbb0b124ed7625
Kashish <kashishkumari363@gmail.com>
2026-09-24 01:17:57 +0530

    Add merge sort

---
+++ merge.java
@@ -0,0 +1,84 @@
+import java.util.*;
+public class merge {
+
+    public static void printArr(int arr[]){
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+    }
+
+    public static void mergeSort(int arr[], int si, int ei){
+        if(si >= ei){
+            return;
+        }
+
+        int mid = si + (ei - si)/2;
+        mergeSort(arr, si, mid);
+        mergeSort(arr, mid+1, ei);
+        Merge(arr, si, mid, ei);
+    }
+    public static void Merge(int arr[], int si, int mid, int ei){
+
+        int temp[] = new int[ei-si+1]
+;
+int i = si;
+int j = mid+1;
+int k = 0;
+
+while(i <= mid && j <= ei) {
+
+    if(arr[i] < arr[j]) {
+        temp[k] = arr[i];
+        i++;
+    }
+    else {
+        temp[k] = arr[j];
+        j++;
+    }
+
+    k++;
+}
+// left part
+while(i <= mid){
+    temp[k++] = arr[i++];
+}
+
+//right part
+while(j <= ei){
+    temp[k++] = arr[j++];
+}
+
+  // Copy temp into original array
+for(k=0, i=si; k<temp.length; k++, i++){
+    arr[i] = temp[k];
+}
+    }
+
+    public static void main(String args[]){
+         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.print("Enter elements:");
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Original array:");
+        printArr(arr);
+
+        mergeSort(arr, 0, arr.length - 1);
+
+        System.out.print("Sorted array:");
+        printArr(arr);
+
+        sc.close();
+    }
+}
+
